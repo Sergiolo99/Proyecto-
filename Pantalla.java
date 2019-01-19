e1c59072e0af5f46bfd38bb6ce6dda4eff77cf49
AdolfoCesar <45486593+AdolfoCesar@users.noreply.github.com>
2019-01-19 18:42:16 +0100

    Add files via upload

---
+++ Pantalla.java
@@ -0,0 +1,50 @@
+package gráficos;
+
+public final class Pantalla {
+
+	private final int ancho;
+	private final int alto;
+
+	public final int[] pixeles;
+
+	// temporal
+	private final static int LADO_SPRITE = 32;
+	private final static int MASCARA_SPRITE = LADO_SPRITE - 1;
+
+	// fin temporal
+
+	public Pantalla(int ancho, int alto) {
+		this.ancho = ancho;
+		this.alto = alto;
+		pixeles = new int[ancho * alto];
+	}
+
+	public void limpiar() {
+		for (int i = 0; i < pixeles.length; i++) {
+			pixeles[i] = 0;
+		}
+	}
+
+	public void mostrar(final int compensacionX, final int compensacionY) {
+		for (int y = 0; y < alto; y++) {
+			int posicionY = y + compensacionY;
+			if (posicionY < 0 || posicionY >= alto) {
+				continue;
+			}
+
+			for (int x = 0; x < ancho; x++) {
+				int posicionX = x + compensacionX;
+				if (posicionX < 0 || posicionX >= ancho) {
+					continue;
+				}
+
+				// temporal
+
+				pixeles[posicionX + posicionY * ancho] = Sprite.asfalto.pixeles[(x & MASCARA_SPRITE)
+						+ (y & MASCARA_SPRITE) * LADO_SPRITE];
+
+			}
+		}
+	}
+
+}
---
+++ Sprite.java
@@ -0,0 +1,35 @@
+package gráficos;
+
+public final class Sprite {
+
+	private final int lado;
+
+	private int x;
+	private int y;
+
+	public int[] pixeles;
+	private final HojaSprites hoja;
+
+	// colección de Sprites
+
+	public static Sprite asfalto = new Sprite(32, 0, 0, HojaSprites.suelo);
+
+	// fin de la colección
+
+	public Sprite(final int lado, final int columna, final int fila, final HojaSprites hoja) {
+		this.lado = lado;
+
+		pixeles = new int[this.lado * this.lado];
+
+		this.x = columna * lado;
+		this.y = fila * lado;
+		this.hoja = hoja;
+
+		for (int y = 0; y < lado; y++) {
+			for (int x = 0; x < lado; x++) {
+				pixeles[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.getAncho()];
+			}
+		}
+	}
+
+}
